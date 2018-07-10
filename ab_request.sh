31ec7aa622d4b6802e676a95417242ea7090893c
zhangzhch2016 <zhangzhch2016@gmail.com>
2018-07-10 18:58:33 +0800

    Update ab_request.sh

--- ab_request.sh
+++ ab_request.sh
@@ -3,7 +3,7 @@ c=50
 n=1000
 domain=http://localhost:8888
 
-token="eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoxMDUsImxvZ2luVGltZSI6IjIwMTgtMDMtMjQgMjI6MjE6NDguMjg5NjQ3In0.IP55U94gKZ3T-zUJcA8pfYPHOZHWWIkDk2elq7qH4gg"
+token="eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoxMDUsImxvZ2luVGltZSI6IjIwMTgtMDUtMTcgMTQ6MDE6MDQuMDc2MjI0In0.yk4Ttv1H0aBDh8w2e7W1QdEAogkl2uZs1YZiuS5r5yM"
 
 # method 类型
 if [ ! -n "$1" ] ;then
@@ -19,19 +19,19 @@ fi
 
 # 并发数量
 if [ -n "$3" ] ;then
-    c=50
+    c=$3
 fi
 
 # 总请求数量
 if [ -n "$4" ] ;then
-    n=1000
+    n=$4
 fi
 echo "$n$c"
 
 if [ $1 == "post" ]; then
-    ab -c ${c} -n ${n} -H 'Authentication:'${token} -p './post.txt' "$token""$2"
+    ab -c ${c} -n ${n} -H 'Authentication:'${token} -p './post.txt' "$domain""$2"
 elif [ $1 == "put" ]; then
-    ab -c ${c} -n ${n} -H 'Authentication:'${token} -u './put.txt' "$token""$2"
+    ab -c ${c} -n ${n} -H 'Authentication:'${token} -u './put.txt' "$domain""$2"
 else
-    ab -c ${c} -n ${n} -H 'Authentication:'${token} "$token""$2"
+    ab -c ${c} -n ${n} -H 'Authentication:'${token} "$domain""$2"
 fi
